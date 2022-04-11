b99e77cb8fd05172daf16e09f07cf890be780626
Gruntion <82691951+Gruntion@users.noreply.github.com>
2022-04-11 14:03:21 -0400

    Add files via upload

---
+++ DataframeTrainingData.py
@@ -0,0 +1,50 @@
+import pandas as pd
+# Example DataFrame Object
+# df = pd.DataFrame("Date", "Company(s)", "Award", "Contract_Type",
+#                  "WhenWorkComplete",
+#                  "ObligatedFunds", "ConditionsForFunds", "Contract#")
+# Info that can be condensed:
+#    Bid_info, Location, For?, WhereWorkComplete, ContractingAgency, FiscalYearFunds,
+#
+#Data Format For Training
+#Example 1
+date = 07012014
+company = ["L-3 Communications Vertex Aerospace LLC"]
+awardamount = 151365660
+contract_type = ["Indefinite-delivery requirements contract"]
+finish_date = 092015
+obligated = 0
+conditions = "Funds will be obligated on individual delivery orders as they are issued"
+contract_number = "N00019-14-D-0011"
+info = [("Madison", "Mississippi"),
+        ("to provide organizational, intermediate, and depot level maintenance and logistics services "
+         "in support of approximately 200 T-45 aircraft based at Naval Air Station Meridian, Mississippi; "
+         "NAS Kingsville, Texas; NAS Pensacola, Florida; and NAS Patuxent River, Maryland. Logistics "
+         "services to be provided include sustaining engineering, supply and government property management, "
+         "and procurement of associated parts and materials."),
+        ("Work will be performed in Kingsville, Texas (48 percent); Meridian, Mississippi (44 percent); "
+         "Pensacola, Florida (7 percent); and Patuxent River, Maryland (1 percent)"),
+        ("Naval Air Systems Command, Patuxent River, Maryland")]
+
+
+date =
+company =
+awardamount =
+contract_type =
+finish_date =
+obligated =
+conditions =
+contract_number =
+info = []
+
+#Column Headers for Data.
+(date, company,  awardamount, contract_type, finish_date, obligated, conditions, contract_number, info)
+
+
+
+
+
+""
+"
+"(Work will be performed in Kingsville, Texas (48 percent); Meridian, Mississippi (44 percent); Pensacola, Florida (7 percent); and Patuxent River, Maryland (1 percent))"
+""
